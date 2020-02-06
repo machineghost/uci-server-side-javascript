4c6c2cb1cb1cbfdfeca6e7103ecf24a119c44f7f
Jeremy Walker <machineghost@gmail.com>
2020-02-06 10:40:38 -0800

    Lots of clean up, through week #6

--- examples/week 6/index.js
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-import { getJoke, getJokes, getRandomJoke } from './jokes'
-import app from './app'
-
-app.listen(8000, () => 
-  console.log(`Listening on port 8000!`));
